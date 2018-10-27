f488168aaa288a648b7ca17b7afc53519b57cf37
lefty55104 <info@pbwebd.com>
2018-10-27 16:56:31 -0400

    Start of main structure

--- assets/process-forms.js
+++ assets/process-forms.js
@@ -2,9 +2,22 @@
 
     $(document).ready(function() {
 
+        /* ***** Scroll Action ***** */
+        $( '.smoothscroll' ).click( function( e ) {
+
+            e.preventDefault();
+
+            var goHere = $( this ).attr( 'href' );
+
+            $( 'html, body' ).animate({
+                scrollTop: $( goHere ).offset().top - 75
+            }, 500 );
+
+        });
+
+        // CHECKBOX MAX 3
         var checkGroup = 0;
         $('.checkbox-max-3').change(function() {
-            console.log('Start: ' + checkGroup);
             if(this.checked) {
                 checkGroup++;
             } else {
@@ -13,41 +26,14 @@
             if(checkGroup > 3){
                 $( this ).prop( "checked", false );
                 checkGroup--;
-                $("#top-3-message").html("You've selected your top 3. Unselect an option to choose this option.");
+                $("#top-3-message").html('<h3 class="notice warning">You\'ve selected your top 3. Unselect an option first.</h3>');
             } else {
                 if(checkGroup == 3){
-                    $("#top-3-message").html("Great! Click the 'Get Matched' button to continue." );
+                    $("#top-3-message").html('<h3 class="notice valid">Great! Click the \'Get Matched\' button to continue.</h3>');
                 } else {
-                    $("#top-3-message").html("Choose up to " + ( 3 - checkGroup ) + " more." );
+                    $("#top-3-message").html('<h3 class="notice valid">Choose up to <span class="amt">' + ( 3 - checkGroup ) + '</span> more interests.</h3>');
                 }
             }
-            console.log('End: ' + checkGroup);
-        });
-
-        $( "#register-interests" ).submit(function( event ) {
-            event.preventDefault();
-
-            // var person = {
-            //     name: $("#id-name").val(),
-            //     address:$("#id-address").val(),
-            //     phone:$("#id-phone").val()
-            // }
-            var person = {
-                name: 'Test'
-            }
-
-            // $('#target').html('sending..');
-
-            $.ajax({
-                url: 'forms/save-forms.php',
-                type: 'post',
-                dataType: 'json',
-                contentType: 'application/json',
-                success: function (data) {
-                    // $('#target').html(data.msg);
-                },
-                data: JSON.stringify(person)
-            });
         });
 
     });
--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta name="viewport" content="width=device-width" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>sImpact - Social Good Matchmaking</title>
+    <title>SoPact - Making an impact through social connection</title>
     
+    <link href="https://fonts.googleapis.com/css?family=Salsa|Bungee+Inline|Open+Sans" rel="stylesheet">
+
+
     <link rel="stylesheet" href="assets/main.css" type="text/css" />
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
@@ -16,45 +19,74 @@
 
     <div class="site-wrapper">
     
-        <h1>Find Your Cause</h1>
-        <h2>Matching you with organizations that matter the most to you.</h2>
+        <div class="header-wrapper">
+            <div class="header-inner">
+                <h1>SoPact: Find Your Cause</h1>
+                <h2>Matching you with organizations that you care about.</h2>
+                <p>We help you find nonprofits that tackle issues you care about. Make connections with organizations and volunteers. We're making an impact through social connection.</p>
+                <div class="button"><a href="#get-started" class="smoothscroll">Get Started</a></div>
+            </div>
+        </div>
 
         <div class="page-wrapper">
 
-            <div class="area-wrapper">
-                <h3>Start by selecting your top 3 causes</h3>
-                <div id="top-3-message"></div>
+            <div id="get-started" class="area-wrapper">
+                <h3 id="top-3-message" class="form-notification area-title">Start by selecting your top <span class="amt">3</span> interests</h3>
+
                 <form id="register-interests" action="register-activist.php" method="POST">
-                    <fieldset>
+
+                    <div class="input-wrapper">
+
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="animal-rights" value="Animal Rights" class="input-box checkbox-max-3"> <label for="animal-rights" class="label-for-check">Animal Rights</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="animal-rights" value="Animal Rights" class="input-box checkbox-max-3"> <label for="animal-rights" class="label-for-check"><span>Animal Rights</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="gender-equality" value="Gender Equality" class="input-box checkbox-max-3"> <label for="gender-equality" class="label-for-check">Gender Equality</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="gender-equality" value="Gender Equality" class="input-box checkbox-max-3"> <label for="gender-equality" class="label-for-check"><span>Gender Equality</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="housing" value="Housing" class="input-box checkbox-max-3"> <label for="housing" class="label-for-check">Housing</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="housing" value="Housing" class="input-box checkbox-max-3"> <label for="housing" class="label-for-check"><span>Housing</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="poverty" value="Poverty" class="input-box checkbox-max-3"> <label for="poverty" class="label-for-check">Poverty</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="poverty" value="Poverty" class="input-box checkbox-max-3"> <label for="poverty" class="label-for-check"><span>Poverty</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="hunger" value="Hunger" class="input-box checkbox-max-3"> <label for="hunger" class="label-for-check">Hunger</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="hunger" value="Hunger" class="input-box checkbox-max-3"> <label for="hunger" class="label-for-check"><span>Hunger</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="healthcare" value="Healthcare" class="input-box checkbox-max-3"> <label for="healthcare" class="label-for-check">Healthcare</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="healthcare" value="Healthcare" class="input-box checkbox-max-3"> <label for="healthcare" class="label-for-check"><span>Healthcare</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="children" value="Children" class="input-box checkbox-max-3"> <label for="children" class="label-for-check">Children</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="children" value="Children" class="input-box checkbox-max-3"> <label for="children" class="label-for-check"><span>Children</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="performing-arts" value="Performing Arts" class="input-box checkbox-max-3"> <label for="performing-arts" class="label-for-check">Performing Arts</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="performing-arts" value="Performing Arts" class="input-box checkbox-max-3"> <label for="performing-arts" class="label-for-check"><span>Performing Arts</span></label>
+                            </div>
                         </div>
                         <div class="checkbox-wrapper">
-                            <input type="checkbox" name="interests[]" id="environmental-awareness" value="Environmental Awareness" class="input-box checkbox-max-3"> <label for="environmental-awareness" class="label-for-check">Environmental Awareness</label>
+                            <div class="checkbox-inner">
+                                <input type="checkbox" name="interests[]" id="environmental-awareness" value="Environmental Awareness" class="input-box checkbox-max-3"> <label for="environmental-awareness" class="label-for-check"><span>Environmental Awareness</span></label>
+                            </div>
                         </div>
-                    </fieldset>
+                    
+                    </div>
+
                     <input type="submit" value="Get Matched">
+
                 </form>
             </div>
 
