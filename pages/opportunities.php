75fbc6f36a47541de7ea52b699f545b9b38dd948
Jason B. Knaster <dev.jasonknaster@gmail.com>
2018-10-27 19:06:34 -0400

    Connects to db

--- pages/opportunities.php
+++ pages/opportunities.php
@@ -13,4 +13,25 @@ $res = $client->request('GET', 'https://hackicims.com/api/v1/companies/72/jobs',
 $data = json_decode($res->getBody());
 
 
-"INSERT INTO opportunity(ID, Title"
+define('DB_SERVER', 'sopact-db.c0enhg7bdhdx.us-east-2.rds.amazonaws.com:3306');
+define('DB_USERNAME', 'SOPACT_ADMIN');
+define('DB_PASSWORD', 'SO_HACK102718');
+define('DB_DATABASE', 'sopact-db');
+
+
+$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
+$database = mysql_select_db(DB_DATABASE) or die(mysql_error());
+
+$sql = "SELECT * from nonprofits
+
+
+$result = mysqli_query($connection, $sql);
+
+while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    echo "<br>";
+    echo "ID: " . $row["ID"]. "<br>";
+	
+}
+     echo "Fetched data successfully\n";
+     //$result = $conn->query($query);
+?>
