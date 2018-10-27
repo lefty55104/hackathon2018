d77d5b1e200ca66af86c1883708e4be3da1fd681
lefty55104 <info@pbwebd.com>
2018-10-27 11:19:10 -0400

    Initial Test Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>SoConn - Social Good Matchmaking</title>
+	<link rel="stylesheet" href="assets/main.css" type="text/css" />
+</head>
+<body>
+
+    <div class="site-wrapper">
+    
+        <h1>Find Your Cause</h1>
+        <h2>Matching you with organizations that matter the most to you.</h2>
+
+        <div class="page-wrapper">
+
+            <div class="area-wrapper">
+                <h3>Start by selecting your top 5 causes</h3>
+            </div>
+
+            <div class="area-wrapper">
+                <div class="area">
+                    <h4>Are you a nonprofit?</h4>
+                    <p><a href="register-nonprofit.php">Register your nonprofit to start finding supporters and volunteers.</a></p>
+                </div>
+                <div class="area">
+                    <h4>Already connected?</h4>
+                    <p><a href="dashboard.php">Log into your account.</a></p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+	
+</body>
+</html>
